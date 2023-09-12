7139d697d274c423cca5dfaf2854271828912010
Charlie <131938948+0xFE0F@users.noreply.github.com>
2023-09-11 19:17:44 -0700

    Update getlinks.js

--- js/getlinks.js
+++ js/getlinks.js
@@ -1,3 +1,12 @@
 import data from '/json/links.json' assert { type: 'json' };
 
 console.log(data);
+
+for(var i = 0; i < data.length; i++) {
+    var link = document.createElement("a");
+  
+    link.innerHTML = data[i].display;
+    link.href = document.URL.concat('/').concat(data[i].page);
+  
+    document.body.appendChild(link);
+}
