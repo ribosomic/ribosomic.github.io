32861f3ac2f71521c85a292c6129ea27595ba662
Charlie <131938948+0xFE0F@users.noreply.github.com>
2023-09-11 19:26:16 -0700

    Update getlinks.js

--- js/getlinks.js
+++ js/getlinks.js
@@ -11,7 +11,7 @@ for(var i = 0; i < data.length; i++) {
     var link = document.createElement("a");
   
     link.innerHTML = data[i].display;
-    link.href = document.URL.concat('/').concat(data[i].page);
+    link.href = document.URL.concat(data[i].page);
   
     document.body.appendChild(link);
 }
